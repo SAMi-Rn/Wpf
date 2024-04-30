85c054b9ace70ba7ffafad38d5884fdb77412723
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-04-29 21:47:31 -0700

    mainview with the customer view; the Dashboard is not ready yet

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Login.Model;
+﻿using FontAwesome.Sharp;
+using Login.Model;
 using Login.Repositories;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 
 namespace Login.ViewModel
@@ -14,6 +16,9 @@ namespace Login.ViewModel
      public class MainViewModel : ViewModel
      {
           private UserAccModel _currentUserAccount;
+          private ViewModel _currentChildView;
+          private string _caption;
+          private IconChar _icon;
           private IUserRepository userRepository;
 
           public UserAccModel CurrentUserAccount
@@ -30,11 +35,45 @@ namespace Login.ViewModel
                }
           }
 
+          public ViewModel CurrentChildView {
+               get {
+                     return _currentChildView;
+               }
+               set { _currentChildView = value; 
+                    OnPropertyChanged(nameof(CurrentChildView));
+
+               }
+          }
+          public string Caption { get {  return _caption; } set { _caption = value; OnPropertyChanged(nameof(Caption)); } }
+          public IconChar Icon { get { return _icon; } set { _icon = value; OnPropertyChanged(nameof(Icon)); } }
+
+          public ICommand ShowHomeViewCommand { get; }
+          public ICommand ShowCustomerViewCommand {  get; }
+
           public MainViewModel()
           {
                userRepository = new UserRepository();
                CurrentUserAccount = new UserAccModel();
+
+               ShowHomeViewCommand = new RelayCommand(ExecuteShowHomeViewCommand);
+               ShowCustomerViewCommand = new RelayCommand(ExecuteShowCustomerViewCommand);
+
+               ExecuteShowHomeViewCommand(null);
                LoadCurrentUserData();
+          }
+
+          private void ExecuteShowCustomerViewCommand(object obj)
+          {
+               CurrentChildView = new CustomerViewModel();
+               Caption = "Customer";
+               Icon = IconChar.UserGroup;
+          }
+
+          private void ExecuteShowHomeViewCommand(object obj)
+          {
+               CurrentChildView = new HomeViewModel();
+               Caption = "Dashboard";
+               Icon = IconChar.Home;
           }
 
           private void LoadCurrentUserData()
@@ -51,7 +90,7 @@ namespace Login.ViewModel
                {
                     CurrentUserAccount.Username = user.Username;
                     CurrentUserAccount.DisplayName = $"Welcome {user.Name} {user.LastName};";
-                    CurrentUserAccount.ProfilePicture = null; // Assuming you might later set this
+                    CurrentUserAccount.ProfilePicture = null;
                }
                else
                {
